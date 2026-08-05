7165a2d5a9f3cd6a3914316086f0b0b65b99664f
Liam <thejoyliam@gmail.com>
2026-08-05 11:45:21 +0800

    release: Bonk v2026.1.1

--- website/assets/js/download.js
+++ website/assets/js/download.js
@@ -9,7 +9,7 @@
   "use strict";
 
   // 版本号（与 appcast.xml 同步，发布新版时改这里一处）
-  const VERSION = "2026.1.0";
+  const VERSION = "2026.1.1";
 
   const DOWNLOADS = {
     arm: `https://github.com/iamJoyLiam/Bonk/releases/download/v${VERSION}/Bonk-${VERSION}-arm64.dmg`,
